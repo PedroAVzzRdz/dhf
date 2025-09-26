35ef972d0db7c4aa31c25c77711a01e0326903fb
Emilio.Ramirez <emiliormz.2005@hotmail.com>
2025-09-26 17:25:20 -0600

    "Commit inicial"

---
+++ app/src/main/java/com/example/dulcehorno/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dulcehorno;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView greetingText = findViewById(R.id.greetingText);
+        greetingText.setText("Hello Android!");
+    }
+}
